e92c8703128f67e209c9aea8cb34b9c9a30d20e7
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-16 15:39:15 +0400

    All numbers

--- src/GameOfRoulette/FromUser.java
+++ src/GameOfRoulette/FromUser.java
@@ -9,11 +9,19 @@ public class FromUser {
             this.cash = cash;
         }
 
-     public void checkCash(){
+     public void checkUserStatus(){
             if(this.cash == 0){
                 System.out.println("You had lost all your money");
                 System.exit(0);
             }
+     }
+
+     public boolean checkCash(int forCheckBidStatus){
+            if(this.cash < forCheckBidStatus){
+                System.out.println("Sorry, but you have no so much money");
+                return false;
+            }
+            return true;
      }
 
     public void counterPlus(int cash){
