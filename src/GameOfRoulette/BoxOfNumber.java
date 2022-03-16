be3a491f48ac4527089bedd865fe0b900454415b
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-16 13:27:01 +0400

    The Roulette game

---
+++ src/GameOfRoulette/BoxOfNumber.java
@@ -0,0 +1,24 @@
+package GameOfRoulette;
+
+public class BoxOfNumber {
+    BoxOfNumber(int number, String quickBets, String color){
+        this.number = number;
+        this.quickBets = quickBets;
+        this.color = color;
+    }
+
+    private final  int number;
+    private final String quickBets;
+    private final String color;
+
+    public int getNumber(){
+        return number;
+    }
+}
+
+/*
+class VoisinsDeZero{
+
+    private final int[] quickBetsBox = {22, 18, 29, 7, 28, 12, 35, 3, 26, 0, 32, 15, 19, 4, 21, 2, 25};
+}
+*/
---
+++ src/GameOfRoulette/Sectors.java
@@ -0,0 +1,48 @@
+package GameOfRoulette;
+
+import java.util.ArrayList;
+
+public class Sectors {
+    public  void addIntegers(){
+        ArrayList<BoxOfNumber> allNumbers = new ArrayList<BoxOfNumber>(37);
+        allNumbers.add(new BoxOfNumber(22, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(18, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(29, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(7, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(28, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(12, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(35, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(3, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(26, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(32, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(15, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(19, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(4, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(21, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(2, "Voisins de zero", "black"));
+        allNumbers.add(new BoxOfNumber(25, "Voisins de zero", "red"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+    }
+
+
+}
