435f37f7da7f2e18b77eb11c4c3f48a47b97f93f
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-16 13:46:01 +0400

    All numbers

--- src/GameOfRoulette/Sectors.java
+++ src/GameOfRoulette/Sectors.java
@@ -22,26 +22,26 @@ public class Sectors {
         allNumbers.add(new BoxOfNumber(21, "Voisins de zero", "red"));
         allNumbers.add(new BoxOfNumber(2, "Voisins de zero", "black"));
         allNumbers.add(new BoxOfNumber(25, "Voisins de zero", "red"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
-        allNumbers.add(new BoxOfNumber(0, "Voisins de zero", "green"));
+        allNumbers.add(new BoxOfNumber(17, "Orphelins", "black"));
+        allNumbers.add(new BoxOfNumber(34, "Orphelins", "red"));
+        allNumbers.add(new BoxOfNumber(6, "Orphelins", "black"));
+        allNumbers.add(new BoxOfNumber(27, "Tier", "red"));
+        allNumbers.add(new BoxOfNumber(13, "Tier", "black"));
+        allNumbers.add(new BoxOfNumber(36, "Tier", "red"));
+        allNumbers.add(new BoxOfNumber(11, "Tier", "black"));
+        allNumbers.add(new BoxOfNumber(30, "Tier", "red"));
+        allNumbers.add(new BoxOfNumber(8, "Tier", "black"));
+        allNumbers.add(new BoxOfNumber(23, "Tier", "red"));
+        allNumbers.add(new BoxOfNumber(10, "Tier", "black"));
+        allNumbers.add(new BoxOfNumber(5, "Tier", "red"));
+        allNumbers.add(new BoxOfNumber(24, "Tier", "black"));
+        allNumbers.add(new BoxOfNumber(16, "Tier", "red"));
+        allNumbers.add(new BoxOfNumber(33, "Tier", "black"));
+        allNumbers.add(new BoxOfNumber(1, "Orphelins", "red"));
+        allNumbers.add(new BoxOfNumber(20, "Orphelins", "black"));
+        allNumbers.add(new BoxOfNumber(14, "Orphelins", "red"));
+        allNumbers.add(new BoxOfNumber(31, "Orphelins", "black"));
+        allNumbers.add(new BoxOfNumber(9, "Orphelins", "red"));
     }
 
 
